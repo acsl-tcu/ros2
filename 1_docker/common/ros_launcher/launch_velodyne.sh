b641f2e42ad9916f9241d99bf95d035317dc7f12
kasekiguchi <kasekiguchi@gmail.com>
2024-10-08 15:56:59 +0900

    whill

--- 1_docker/common/ros_launcher/launch_velodyne.sh
+++ 1_docker/common/ros_launcher/launch_velodyne.sh
@@ -1,26 +1,21 @@
 #! /usr/bin/bash
 
-if [[ ! "${TAG}" == image_* ]]; then
-  echo "Build first"
-  bash
-else
-  # https://qiita.com/porizou1/items/c815cb17749aad561909
-  #  $(echo "exec ros2 launch velodyne velodyne-all-nodes-VLP16-launch.py")
+# https://qiita.com/porizou1/items/c815cb17749aad561909
+#  $(echo "exec ros2 launch velodyne velodyne-all-nodes-VLP16-launch.py")
 
-  # https://qiita.com/miriwo/items/e829f5a78314e0878f1b
-  case $1 in
-  "run")
-    $(echo "exec ros2 run velodyne_driver velodyne_driver_node --ros-args -p device_ip:='192.168.1.22' -p model:='VLP16' -p rpm:=1200.0")
-    ;;
-  #ROS2のPCL2形式に変換するノードのようです。
-  "convert")
-    $(echo "exec ros2 launch velodyne_pointcloud velodyne_convert_node-VLP16-launch.py")
-    ;;
-  "rviz2")
-    $(echo "exec rviz2")
-    ;;
-  esac
-fi
+# https://qiita.com/miriwo/items/e829f5a78314e0878f1b
+case $1 in
+"run")
+  $(echo "exec ros2 run velodyne_driver velodyne_driver_node --ros-args -p device_ip:='192.168.1.22' -p model:='VLP16' -p rpm:=1200.0")
+  ;;
+#ROS2のPCL2形式に変換するノードのようです。
+"convert")
+  $(echo "exec ros2 launch velodyne_pointcloud velodyne_convert_node-VLP16-launch.py")
+  ;;
+"rviz2")
+  $(echo "exec rviz2")
+  ;;
+esac
 
 # frameをvelodyneに設定
 # pointclound2を選択して/velodyne_pointsを開く
