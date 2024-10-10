de91c6030035e726f8ef5455379f434d2f2eaf6b
kasekiguchi <kasekiguchi@gmail.com>
2024-10-10 09:33:06 +0900

    Update launch_velodyne.sh

--- 1_docker/common/ros_launcher/launch_velodyne.sh
+++ 1_docker/common/ros_launcher/launch_velodyne.sh
@@ -6,7 +6,7 @@
 # https://qiita.com/miriwo/items/e829f5a78314e0878f1b
 case $1 in
 "run")
-  $(echo "exec ros2 run velodyne_driver velodyne_driver_node --ros-args -p device_ip:='192.168.1.22' -p model:='VLP16' -p rpm:=1200.0;exec ros2 launch velodyne_pointcloud velodyne_transform_node-VLP16-launch.py")
+  $(echo "exec ros2 run velodyne_driver velodyne_driver_node --ros-args -p device_ip:='192.168.1.22' -p model:='VLP16' -p rpm:=1200.0; ros2 launch velodyne_pointcloud velodyne_transform_node-VLP16-launch.py")
   ;;
 #ROS2のPCL2形式に変換するノードのようです。
 "convert")
