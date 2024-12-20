3226f107a6ca5e882961621a4285175aba98e868
kasekiguchi <kasekiguchi@gmail.com>
2024-12-20 13:20:49 +0900

    colcon build on the image

--- 1_launcher/launch_build.sh
+++ 1_launcher/launch_build.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/bash
 
-# Check if /usr/bin/bash exists
-# if [ -x "/usr/bin/bash" ]; then
-#   exec /usr/bin/bash "$@"
-# # Check if /bin/bash exists
-# elif [ -x "/bin/bash" ]; then
-#   exec /bin/bash "$@"
-# else
-#   echo "Bash not found in /usr/bin/bash or /bin/bash"
-#   exit 1
-# fi
-
 source /common/scripts/super_echo
-becho "colcon build" $@
+becho "lunch_build: colcon build $@"
 cd /root/ros2_ws
 source /opt/ros/$ROS_DISTRO/setup.bash
 if [ $# -ge 1 ]; then
