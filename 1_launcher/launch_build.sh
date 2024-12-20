5b3f717a8e2c06510249350e99d5b157666ca803
kasekiguchi <kasekiguchi@gmail.com>
2024-12-20 12:50:26 +0900

    Update launch_build.sh

--- 1_launcher/launch_build.sh
+++ 1_launcher/launch_build.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/usr/bin/bash
 
 # Check if /usr/bin/bash exists
-if [ -x "/usr/bin/bash" ]; then
-  exec /usr/bin/bash "$@"
-# Check if /bin/bash exists
-elif [ -x "/bin/bash" ]; then
-  exec /bin/bash "$@"
-else
-  echo "Bash not found in /usr/bin/bash or /bin/bash"
-  exit 1
-fi
+# if [ -x "/usr/bin/bash" ]; then
+#   exec /usr/bin/bash "$@"
+# # Check if /bin/bash exists
+# elif [ -x "/bin/bash" ]; then
+#   exec /bin/bash "$@"
+# else
+#   echo "Bash not found in /usr/bin/bash or /bin/bash"
+#   exit 1
+# fi
 
 source /common/scripts/super_echo
 becho "colcon build" $@
