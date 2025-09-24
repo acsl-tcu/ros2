4726242346488ec34ebe509ee61e9ff9d1ecb7f4
kasekiguchi <kasekiguchi@gmail.com>
2025-09-24 18:32:35 +0900

    colcon build

--- 1_launcher/launch_build.sh
+++ 1_launcher/launch_build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
-
+source /root/.venv/bin/activate
 source /common/scripts/super_echo
 gecho "LAUNCH BUILD: ${ROS_DOMAIN_ID}"
 cd /root/ros2_ws
 source /opt/ros/$ROS_DISTRO/setup.bash
 if [ $# -ge 1 ]; then
   echo "  colcon build --symlink-install --packages-select $@ --parallel-workers $(($(nproc) - 1))"
-  colcon build --symlink-install --packages-select $@ --parallel-workers $(($(nproc) - 1))
+  python3 -m colcon build --symlink-install --packages-select $@ --parallel-workers $(($(nproc) - 1))
 else
   echo "  colcon build --symlink-install --parallel-workers $(($(nproc) - 1))"
-  colcon build --symlink-install --parallel-workers $(($(nproc) - 1))
+  python3 -m colcon build --symlink-install --parallel-workers $(($(nproc) - 1))
 fi
 source install/setup.bash
