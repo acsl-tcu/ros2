40cb91a0fb1a57cfe19df214af918790c800ded5
kasekiguchi <kasekiguchi@gmail.com>
2025-10-09 18:07:22 +0900

    Update launch_build.sh

--- 1_launcher/launch_build.sh
+++ 1_launcher/launch_build.sh
@@ -6,9 +6,9 @@ cd /root/ros2_ws
 source /opt/ros/$ROS_DISTRO/setup.bash
 if [ $# -ge 1 ]; then
   echo "  colcon build --symlink-install --packages-select $@ --parallel-workers $(($(nproc) - 1))"
-  python3 -m colcon build --symlink-install --packages-select $@ --parallel-workers $(($(nproc) - 1))
+  colcon build --symlink-install --packages-select $@ --parallel-workers $(($(nproc) - 1))
 else
   echo "  colcon build --symlink-install --parallel-workers $(($(nproc) - 1))"
-  python3 -m colcon build --symlink-install --parallel-workers $(($(nproc) - 1))
+  colcon build --symlink-install --parallel-workers $(($(nproc) - 1))
 fi
 source install/setup.bash
