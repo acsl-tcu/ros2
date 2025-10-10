f62b72866262327d9b2e901b2d5c94d62ffb580b
kasekiguchi <kasekiguchi@gmail.com>
2025-10-10 12:35:19 +0900

    Update launch_build.sh

--- 1_launcher/launch_build.sh
+++ 1_launcher/launch_build.sh
@@ -4,13 +4,10 @@ source /common/scripts/super_echo
 gecho "LAUNCH BUILD: ${ROS_DOMAIN_ID}"
 cd /root/ros2_ws
 source /opt/ros/$ROS_DISTRO/setup.bash
-which colcon
-colcon --version
-ls -l /usr/bin/colcon
-/usr/bin/colcon --version
+
 if [ $# -ge 1 ]; then
   echo " colcon build --symlink-install --packages-select $@ --parallel-workers $(($(nproc) - 1))"
-/usr/bin/colcon build --symlink-install --packages-select $@ --parallel-workers $(($(nproc) - 1))
+colcon build --symlink-install --packages-select $@ --parallel-workers $(($(nproc) - 1))
 else
   echo " colcon build --symlink-install --parallel-workers $(($(nproc) - 1))"
 colcon build --symlink-install --parallel-workers $(($(nproc) - 1))
