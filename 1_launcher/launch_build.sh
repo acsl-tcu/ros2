26d00a1a8909cebd52dacb59be4f2d41cc07eb58
kasekiguchi <kasekiguchi@gmail.com>
2025-10-10 12:32:24 +0900

    Update launch_build.sh

--- 1_launcher/launch_build.sh
+++ 1_launcher/launch_build.sh
@@ -7,7 +7,7 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 which colcon
 if [ $# -ge 1 ]; then
   echo " colcon build --symlink-install --packages-select $@ --parallel-workers $(($(nproc) - 1))"
-　colcon build --symlink-install --packages-select $@ --parallel-workers $(($(nproc) - 1))
+　/usr/bin/colcon build --symlink-install --packages-select $@ --parallel-workers $(($(nproc) - 1))
 else
   echo " colcon build --symlink-install --parallel-workers $(($(nproc) - 1))"
   colcon build --symlink-install --parallel-workers $(($(nproc) - 1))
